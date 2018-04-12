27fd0ea70f10dc5bf4f6503fa3924a482515c76b
陈发旺(视频) <fawangchen@sohu-inc.com>
2018-04-12 15:24:19 +0800

    coding build script file

--- build_linux.sh
+++ build_linux.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-
-GYP_TOOLS="/root/gyp/gyp"
-
-
-${GYP_TOOLS} --depth=. -f make wave_trans.gyp
+if [ %# != 2 ]
+then
+  echo "without input gyp tools path"
+  echo "usage: $0 gyp_tools_path"
+else
+  GYP_TOOLS="/root/gyp/gyp"
+  ${GYP_TOOLS} --depth=. -f make wave_trans.gyp
+fi
